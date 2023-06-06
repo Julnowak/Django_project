be6bf6082fdd714dea7cc02cfe57f1ae455cae81
Julnowak <91898403+Julnowak@users.noreply.github.com>
2023-06-06 19:42:54 +0200

    Post adding and editing added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,13 @@ urlpatterns = [
 
     # Entries pages
     path('topics/(<int:topic_id>)/', views.topic, name='topic'),
+
+    # Add new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Add new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Edit entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
 
 # Create your views here.
 def index(request):
@@ -16,6 +18,61 @@ def topics(request):
 def topic(request, topic_id):
     """ Shows topic and all entries"""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by("date_added")
+    entries = topic.entry_set.order_by("-date_added")
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ Add new topic"""
+    if request.method != "POST":
+        # No data, empty form
+        form = TopicForm()
+    else:
+        # data by POST, to work with
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Showing empty form
+    context = {'form': form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request, topic_id):
+    """ Add new entry"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        # No data, empty form
+        form = EntryForm()
+    else:
+        # data by POST, to work with
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic',topic_id=topic_id)
+
+    # Showing empty form
+    context = {'topic': topic,'form': form}
+    return render(request,'learning_logs/new_entry.html',context)
+
+def edit_entry(request, entry_id):
+    """ Edit entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != "POST":
+        # No changes, user can  see text
+        form = EntryForm(instance=entry)
+    else:
+        # data by POST, to work with
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    # Showing empty form
+    context = {'entry': entry, 'topic': topic,'form': form}
+    return render(request,'learning_logs/edit_entry.html',context)
